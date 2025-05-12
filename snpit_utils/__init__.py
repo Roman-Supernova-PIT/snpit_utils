55fb3864d6d0e7976daff9df1d35e18d5217248c
Rob Knop <raknop@lbl.gov>
2025-05-12 13:42:31 -0700

    Creating a roman_snpit_utils package, starting with a config system and a logger system

---
+++ snpit_utils/__init__.py
@@ -0,0 +1,11 @@
+import re
+from importlib.metadata import version, PackageNotFoundError
+__all__ = []
+
+
+
+try:
+    __version__ = version("snpit_utils")
+except PackageNotFoundError:
+    # package is not installed
+    pass
---
+++ snpit_utils/tests/conftest.py
@@ -0,0 +1,5 @@
+# this is a module level file
+import pytest
+
+import tox
+from tox.pytest import init_fixture
