e4be3416edc2eab80afe5172caf5a0a3aea42ac9
rknop <rknop@users.noreply.github.com>
2025-05-28 15:15:18 +0000

    Add API to documentation.
    * documentation edit
    
    * changelog
    
    * prune tests from package 437d5cbe8f432614f08d7d07375f1ff13ca1746b

--- _sources/usage.rst.txt
+++ _sources/usage.rst.txt
@@ -5,3 +5,5 @@ Usage
 To use roman_snpit_utils in a project::
 
     import snpit_utils
+
+Usage of the class is documented in the :ref:`API documentation<api>`.
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev1+g8bfbc0f',
+    VERSION: '0.1.dev1+g437d5cb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
