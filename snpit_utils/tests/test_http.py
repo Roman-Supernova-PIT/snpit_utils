377e394c4e0783b8430b333fe5889c8814b863da
Rob Knop <raknop@lbl.gov>
2025-10-10 16:11:53 -0700

    test fix, rkwebutils version bump

--- snpit_utils/tests/test_http.py
+++ snpit_utils/tests/test_http.py
@@ -16,5 +16,5 @@ def test_retry_post( dbclient ):
     assert data['param'] == 'blah'
     assert data['json']['answer'] == 42
 
-    with pytest.raises( RuntimeError, match="Got status 500 trying to connect" ):
+    with pytest.raises( RuntimeError, match="Got status 404 trying to connect" ):
         retry_post( 'https://webserver:8080/this_endpoint_does_not_exist', retries=3, initsleep=0.2, verify=False )
